d32c657ea55917d34d7a7d1bf10b1f28acf5729a
Daniel-codemaster <dchembcy@gmail.com>
2023-10-02 08:57:54 +0200

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -8,19 +8,10 @@ import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
 Future<void> main() async {
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
   WidgetsFlutterBinding.ensureInitialized();
   await SessionManagers.initialize();
   final settingsController = SettingsController(SettingsService());
-
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
-
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
   runApp(const MainApp());
 }
 
@@ -32,16 +23,9 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Providing a restorationScopeId allows the Navigator built by the
-      // MaterialApp to restore the navigation stack when a user leaves and
-      // returns to the app after it has been killed while running in the
-      // background.
       debugShowCheckedModeBanner: false,
       theme: AppTheme.defaultTheme,
       title: 'Bus Ticketing System',
-      // Provide the generated AppLocalizations to the MaterialApp. This
-      // allows descendant Widgets to display the correct translations
-      // depending on the user's locale.
       localizationsDelegates: const [
         AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
@@ -49,21 +33,9 @@ class MainApp extends StatelessWidget {
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: const [
-        Locale('en', ''), // English, no country code
+        Locale('en', ''),
       ],
       home: const LoginPage(),
-      // Use AppLocalizations to configure the correct application title
-      // depending on the user's locale.
-      //
-      // The appTitle is defined in .arb files found in the localization
-      // directory.
-
-      // Define a light and dark color theme. Then, read the user's
-      // preferred ThemeMode (light, dark, or system default) from the
-      // SettingsController to display the correct theme.
-
-      // Define a function to handle named routes in order to support
-      // Flutter web url navigation and deep linking.
     );
   }
 }
